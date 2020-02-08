b70b68f3b303ae1b2ea59591b948a6307ae89b93
hyer0705 <hyer1205@gmail.com>
2020-02-09 01:23:29 +0900

    created-background

---
+++ src/background.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 6;
+const IMG_CL = "bg-img";
+
+function paintImg(num) {
+    const image = new Image();
+    image.src = `./images/${num + 1}.jpg`;
+    image.classList.add(IMG_CL);
+    body.prepend(image);
+}
+
+function genRandom() {
+    const random = Math.floor(Math.random() * IMG_NUMBER);
+    return random;
+}
+
+function init() {
+    const randomNum = genRandom();
+    paintImg(randomNum);
+}
+init();
